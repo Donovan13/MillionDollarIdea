51b614cf6cfaabb97fbc6a0c0e31e4a1614d6fc4
Ky <ky@Kys-MacBook-Pro.local>
2017-12-05 21:45:53 -0500

    second try

--- Million Dollar Idea/ViewController.swift
+++ Million Dollar Idea/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
-
+    var player = AVAudioPlayer()
+    let path = Bundle.main.path(forResource: "sample", ofType: "mp3")
+    var isPlaying = false
+    
+    @IBOutlet weak var bigImage: UIImageView!
+    @IBOutlet weak var bigButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor .black
+        bigImage.image = UIImage(named:"1.jpg")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+        } catch {
+            print("Could not load file")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func bigButtonPressed(_ sender: Any) {
+        if isPlaying == false {
+            player.play()
+            isPlaying = true
+            bigButton.setTitle("Press Again!", for: .normal)
+            let toImage = UIImage(named:"2.jpg")
+            UIView.transition(with: self.bigImage,
+                              duration:3,
+                              options: .transitionFlipFromTop,
+                              animations: { self.bigImage.image = toImage },
+                              completion: nil)
+        } else {
+            player.play()
+            isPlaying = false
+            bigButton.setTitle("Woo!!!", for: .normal)
+            let toImage = UIImage(named:"3.jpg")
+            UIView.transition(with: self.bigImage,
+                              duration:2,
+                              options: .transitionFlipFromBottom,
+                              animations: { self.bigImage.image = toImage },
+                              completion: nil)
+            
+            
+        }
+        
     }
-
-
 }
-
